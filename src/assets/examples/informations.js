b9443c2cd1c0a00c99ec4cfbf5f9066308b3d6a8
Jérôme Gil <LinkJerome@gmail.com>
2023-01-23 15:35:16 +0100

    Update informations.js

--- src/assets/examples/informations.js
+++ src/assets/examples/informations.js
@@ -1,47 +1,49 @@
-import edm from '../images/edm.jfif';
-import clm from '../images/clm.jfif';
-import jeg from '../images/jeg.jpg';
-import reactIcon from '../../logo.svg';
-import elixirIcon from '../images/elixir.png';
+import edm from '../images/edm.jfif'
+import clm from '../images/clm.jfif'
+import jeg from '../images/jeg.jpg'
+import reactIcon from '../../logo.svg'
+import elixirIcon from '../images/elixir.png'
 
 const claudie = {
-    name: "Claudie Micat",
-    description: null,
-    link: "https://github.com/claudie-micat",
-    at: "@claudie-micat",
-    img: clm
-};
+  name: 'Claudie Micat',
+  description: null,
+  link: 'https://github.com/claudie-micat',
+  at: '@claudie-micat',
+  img: clm
+}
 
 const edouard = {
-    name: "Edouard Menayde",
-    description: "French computer science student who loves open-source and libre software community.",
-    link: "https://github.com/edouardmenayde",
-    at: "@edouardmenayde",
-    img: edm
-};
+  name: 'Edouard Menayde',
+  description:
+    'French computer science student who loves open-source and libre software community.',
+  link: 'https://github.com/edouardmenayde',
+  at: '@edouardmenayde',
+  img: edm
+}
 
 const jerome = {
-    name: "Jérôme Gil",
-    description: "Étudiant en informatique à l’UCBL, j'aime travailler sur des projets d’Art, d'Accessibilité ou de Géomatique.",
-    link: "https://github.com/LinkJerome",
-    at: "@LinkJerome",
-    img: jeg
-};
+  name: 'Jey Puget Gil',
+  description:
+    "Étudiant en informatique à l’UCBL, j'aime travailler sur des projets d’Art, d'Accessibilité ou de Géomatique.",
+  link: 'https://github.com/LinkJerome',
+  at: '@LinkJerome',
+  img: jeg
+}
 
 const reactRepo = {
-    name: "Repository React",
-    description: "Demo d'utilisation de la librairie React.js.",
-    link: "https://github.com/LinkJerome/react-demo",
-    at: "@LinkJerome/react-demo",
-    img: reactIcon
-};
+  name: 'Repository React',
+  description: "Demo d'utilisation de la librairie React.js.",
+  link: 'https://github.com/LinkJerome/react-demo',
+  at: '@LinkJerome/react-demo',
+  img: reactIcon
+}
 
 const elixirRepo = {
-    name: "Repository Elixir",
-    description: "Repository de la websocket Elixir du projet react-demo.",
-    link: "https://github.com/edouardmenayde/ws-counter",
-    at: "@edouardmenayde/ws-counter",
-    img: elixirIcon
-};
+  name: 'Repository Elixir',
+  description: 'Repository de la websocket Elixir du projet react-demo.',
+  link: 'https://github.com/edouardmenayde/ws-counter',
+  at: '@edouardmenayde/ws-counter',
+  img: elixirIcon
+}
 
-export default {claudie, edouard, jerome, reactRepo, elixirRepo};
+export default { claudie, edouard, jerome, reactRepo, elixirRepo }
